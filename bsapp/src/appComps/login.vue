ffe29c4df9ab1c0aeaa72a5c0552ceea2771e08f
董先生 <3057747115@qq.com>
2022-02-25 20:25:26 +0800

    '完成了登录功能'

--- bsapp/src/appComps/login.vue
+++ bsapp/src/appComps/login.vue
@@ -1,30 +1,29 @@
 <template>
     <div class="loginPage">        
         <div class="divTitle">
-            斗鸡视频管理系统
+            国家呼吸研医教协同管理系统
         </div>
-        <div class="divData">
-            <div class="divRow">                    
-                <div class="divLabel">
-                    用户名
-                </div>
-                <div class="divEditor">
-                    <el-input v-model="userCode" placeholder="请输入用户名"></el-input>
-                </div> 
+        <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="80px">
+            <div class="divData">
+                <el-form-item label="用户名" prop="username" class="divEditor">
+                    <el-input v-model="loginForm.username" placeholder="请输入用户名">
+                        <i slot="prefix" class="el-input__icon el-icon-user"></i>
+                    </el-input>
+                </el-form-item>
+
+                <el-form-item label="密码" prop="password" class="divEditor">
+                    <el-input v-model="loginForm.password" type="password" placeholder="请输入密码">
+                        <i slot="prefix" class="el-input__icon el-icon-lock"></i>
+                    </el-input>
+                </el-form-item>
+
+                <el-form-item class="divBtns">
+                    <el-button type="text">忘记密码</el-button>
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
             </div>
-            <div class="divRow">                    
-                <div class="divLabel">
-                    密码
-                </div>
-                <div class="divEditor">
-                    <el-input v-model="userPwd" type="password" placeholder="请输入密码"></el-input>
-                </div> 
-            </div>
-            <div class="divBtns">      
-                <el-button type="text">忘记密码</el-button>
-                <el-button type="primary">登录系统</el-button>
-            </div>
-        </div>
+        </el-form>
 
     </div>
 </template>
@@ -33,9 +32,48 @@
     export default {
         data(){
             return {
-                userCode:"admin",
-                userPwd:""
+                //这是登录表单的数据绑定对象
+                loginForm:{
+                    username:"admin",
+                    password:"123456"
+                },
+                //这是表单的验证规则对象
+                loginFormRules:{
+                    //验证用户名是否合法
+                    username:[
+                      {required: true, message:'请输入用户名', trigger: 'blur' },
+                      { min: 3, max: 10, message: '长度在 3 到 10 个字符之间', trigger: 'blur' }
+                    ],
+                    //验证密码是否合法
+                    password:[
+                        {required: true, message:'请输入密码', trigger: 'blur' },
+                        { min: 3, max: 15, message: '长度在 3 到 15 个字符之间', trigger: 'blur' }
+                    ],
+                }
             };
+        },
+        methods:{
+            //点击重置按钮，重置登录表单
+            resetLoginForm(){
+                this.$refs.loginFormRef.resetFields();
+            },
+            //表单预验证
+            login(){
+                this.$refs.loginFormRef.validate(async valid => {
+                    //若不合法，不发起请求
+                    if (!valid) return;
+                    const {data:res} = await this.$http.post('login',this.loginForm);
+                    if (res.meta.status !== 200) return this.$message.error('登录失败');
+                    this.$message.success('登录成功');
+                //    1.将登录成功之后的token保存到客户端的sessionStorage
+                //      1.1 项目中除了登录之外的其他API接口，必须在登陆之后才能访问
+                //      1.2 token只应在网站打开期间生效，所以将token保存在sessionStorage 中
+                    window.sessionStorage.setItem("token",res.data.token);
+                //    2.通过编程式导航跳转到后台主页，路由地址是 /home
+                    this.$router.push('/home');
+
+                });
+            }
         }
     }
 </script>
@@ -59,18 +97,6 @@
 
         .divData{
             padding: 15px;
-
-            .divRow{
-                display: flex;
-                padding: 10px 0px;
-                align-items: center;
-                .divLabel{
-                    width: 100px;
-                    padding-right: 10px;
-                    text-align: right;
-                }
-            }
-
             .divBtns{
                 padding-top: 15px;
                 display: flex;
--- bsapp/src/main.js
+++ bsapp/src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+import axios from 'axios'
+Vue.prototype.$http = axios
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 
